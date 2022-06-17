a02cfb89e2dc975361155239b50383fe45d50673
Augusto <augustinhobrito542@gmail.com>
2022-06-17 17:42:21 -0300

    exobe um artigo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require 'config.php';
 
-include 'Artigo.php';
+include 'src/Artigo.php';
 $artigo = new Artigo($mysql);
 $artigos = $artigo->exibeTodosArtigos();
 
@@ -21,7 +21,7 @@ $artigos = $artigo->exibeTodosArtigos();
         <h1>Meu Blog</h1>
         <?php foreach ($artigos as $artigo) : ?>
         <h2>
-            <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+            <a href="artiigo.php?id=<?php echo $artigo['id']; ?>">
                 <?php echo $artigo['titulo']; ?>
             </a>
         </h2>
