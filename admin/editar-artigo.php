2cab1d0622c3a9e22247e928bcd1e4dbb3a7d7df
Augusto <augustinhobrito542@gmail.com>
2022-07-04 12:12:10 -0300

    atualiza artigos no bd

--- admin/editar-artigo.php
+++ admin/editar-artigo.php
@@ -4,6 +4,13 @@ require '../config.php';
 include '../src/Artigo.php';
 require '../src/redireciona.php';
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $artigo = new Artigo($mysql);
+    $artigo->editarArtigo($_POST["id"], $_POST["titulo"], $_POST["conteudo"]);
+
+    redireciona('/blog/admin/index.php');
+}
+
 $artigo = new Artigo($mysql);
 $art = $artigo->encontrarUmArtigoPorId($_GET["id"]);
 
@@ -23,17 +30,17 @@ $art = $artigo->encontrarUmArtigoPorId($_GET["id"]);
 <body>
     <div id="container">
         <h1>Editar Artigo</h1>
-        <form action="editar-artigo.html" method="post">
+        <form action="editar-artigo.php" method="post">
             <p>
                 <label for="titulo">Digite o novo título do artigo</label>
-                <input class="campo-form" type="text" name="titulo" id="titulo" value="<?php echo $art["titulo"];?>" />
+                <input class="campo-form" type="text" name="titulo" id="titulo" value="<?php echo $art["titulo"]; ?>" />
             </p>
             <p>
                 <label for="conteudo">Digite o novo conteúdo do artigo</label>
-                <textarea class="campo-form" type="text" name="conteudo" id="titulo"><?php echo $art["conteudo"];?></textarea>
+                <textarea class="campo-form" type="text" name="conteudo" id="titulo"><?php echo $art["conteudo"]; ?></textarea>
             </p>
             <p>
-                <input type="hidden" name="id" value="" />
+                <input type="hidden" name="id" value="<?php echo $art["id"]; ?>" />
             </p>
             <p>
                 <button class="botao">Editar Artigo</button>
